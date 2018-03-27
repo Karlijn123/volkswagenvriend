1f51ae44cb8677c125136dd39fb18af0a7c1c0bb
Karlijn Engels <karlijn.engels@hotmail.com>
2018-03-27 11:53:42 +0200

    Minimum cases for project

--- server.js
+++ server.js
@@ -92,68 +92,12 @@ io.on('connection', function(socket){
                     console.log("intro action triggered");
                     socket.emit('intro', { description: currentAction.action});
                     break;
-                case "start":
-                    console.log("start action triggered");
-                    socket.emit('start', { description: currentAction.action});
-                    break;
+                
                 case "research":
                     console.log("research action trigggerd!");
                     socket.emit('research', { description: currentAction.action});
                     break;
-                case "result":
-                    console.log("result action trigggerd!");
-                    socket.emit('result', { description: currentAction.action});
-                    break;
-                case "sassy":
-                    console.log("sassy action trigggerd!");
-                    socket.emit('sassy', { description: currentAction.action});
-                    break;
-                case "joke":
-                    console.log("joke action trigggerd!");
-                    socket.emit('joke', { description: currentAction.action});
-                    break;
-                case "benefits":
-                    console.log("benefits action trigggerd!");
-                    socket.emit('benefits', { description: currentAction.action});
-                    break;
-                case "home":
-                    console.log("home action trigggerd!");
-                    socket.emit('home', { description: currentAction.action});
-                    break;
-                case "agenda":
-                    console.log("agenda action trigggerd!");
-                    socket.emit('agenda', { description: currentAction.action});
-                    break;
-                case "file":
-                    console.log("file action trigggerd!");
-                    socket.emit('file', { description: currentAction.action});
-                    break;
-                case "fysio":
-                    console.log("fysio action trigggerd!");
-                    socket.emit('fysio', { description: currentAction.action});
-                    break;
-                case "recepten":
-                    console.log("recepten action trigggerd!");
-                    socket.emit('recepten', { description: currentAction.action});
-                    break;
-                case "spongebob":
-                    console.log("spongebob action trigggerd!");
-                    socket.emit('spongebob', { description: currentAction.action});
-                    break;
-                case "conclusion":
-                    console.log("conclusion action trigggerd!");
-                    socket.emit('conclusion', { description: currentAction.action});
-                    break;
-
-
-                case "cookie":
-                    console.log("cookie action trigggerd!");
-                    socket.emit('cookie', { description: currentAction.action});
-                    break;
-                case "LookAtMenu":
-                    console.log("look at menu action triggered");
-                    socket.emit('menu', { description: currentAction.action});
-                    break;
+               
 
 
                 default:
